f143475f23e592e880a148521d663c100fbff6f3
darkphoenix17 <karanmistry284@gmail.com>
2021-11-06 20:54:07 +0530

    Logs Added

--- project/techtrends/app.py
+++ project/techtrends/app.py
@@ -1,7 +1,15 @@
 import sqlite3
+import logging
+import sys
 
 from flask import Flask, jsonify, json, render_template, request, url_for, redirect, flash
 from werkzeug.exceptions import abort
+
+COUNT = 0
+
+def hit_counter():
+    global COUNT
+    COUNT = COUNT + 1
 
 # Function to get a database connection.
 # This function connects to database with the name `database.db`
@@ -13,8 +21,7 @@ def get_db_connection():
 # Function to get a post using its ID
 def get_post(post_id):
     connection = get_db_connection()
-    post = connection.execute('SELECT * FROM posts WHERE id = ?',
-                        (post_id,)).fetchone()
+    post = connection.execute('SELECT * FROM posts WHERE id = ?',(post_id,)).fetchone()
     connection.close()
     return post
 
@@ -36,13 +43,17 @@ def index():
 def post(post_id):
     post = get_post(post_id)
     if post is None:
+      app.logger.error('No article found')
       return render_template('404.html'), 404
     else:
+      title = tuple(post)[2]
+      app.logger.info('Article /"%s" retrieved!', title)
       return render_template('post.html', post=post)
 
 # Define the About Us page
 @app.route('/about')
 def about():
+    app.logger.info('/about page successfully retrieved!')
     return render_template('about.html')
 
 # Define the post creation functionality 
@@ -53,14 +64,14 @@ def create():
         content = request.form['content']
 
         if not title:
+            app.logger.info('title required!')
             flash('Title is required!')
         else:
             connection = get_db_connection()
-            connection.execute('INSERT INTO posts (title, content) VALUES (?, ?)',
-                         (title, content))
+            connection.execute('INSERT INTO posts (title, content) VALUES (?, ?)',(title, content))
             connection.commit()
             connection.close()
-
+            app.logger.info('Article \"%s" successfully created!', title)
             return redirect(url_for('index'))
 
     return render_template('create.html')
@@ -72,7 +83,7 @@ def status():
         status=200,
         mimetype='application/json'
     )
-    #app.logger.info('Status request successfull')
+    app.logger.info('Status request successfull')
     return response
 
 @app.route("/metrics")
@@ -85,9 +96,17 @@ def metric():
             status=200,
             mimetype='application/json'
     )
-    #app.logger.info('Metrics request successfull')
+    app.logger.info('Metrics request successfull')
     return response
 
 # start the application on port 3111
 if __name__ == "__main__":
+   # stream logs
+   stdout_handler = logging.StreamHandler(sys.stdout)
+   stderr_handler = logging.StreamHandler(sys.stderr)
+   handlers = [stderr_handler, stdout_handler]
+   # format output
+   format_output = '%(asctime)s %(levelname)s %(name)s %(threadName)s : %(message)s'
+   logging.basicConfig(level=logging.DEBUG, handlers=handlers, format=format_output)
+
    app.run(host='0.0.0.0', port='3111')
